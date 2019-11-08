723e8500a6bcfb2f9f895299ab722420779fa775
DGinder <darrin.ginder@yahoo.com>
2019-11-08 15:02:42 -0800

    Finished Message system

--- DatingApp-SPA/src/app/members/member-messages/member-messages.component.ts
+++ DatingApp-SPA/src/app/members/member-messages/member-messages.component.ts
@@ -4,6 +4,7 @@ import { User } from 'src/app/_models/user';
 import { Message } from 'src/app/_models/message';
 import { UserService } from 'src/app/_services/user.service';
 import { AlertifyService } from 'src/app/_services/alertify.service';
+import { tap } from 'rxjs/operators';
 
 @Component({
   selector: 'app-member-messages',
@@ -23,7 +24,18 @@ export class MemberMessagesComponent implements OnInit {
   }
 
   loadMessages() {
-    this.userService.getMessageThread(this.authService.decodedToken.nameid, this.recipientId)
+    const currentUserId = +this.authService.decodedToken.nameid;
+    this.userService.getMessageThread(currentUserId, this.recipientId)
+      .pipe(
+        tap(messages => {
+          // tslint:disable-next-line: prefer-for-of
+          for (let i = 0; i < messages.length; i++) {
+            if (messages[i].isRead === false && messages[i].recipientId === currentUserId){
+              this.userService.markAsRead(currentUserId, messages[i].id);
+            }
+          }
+        })
+      )
       .subscribe(messages => {
         this.messages = messages;
       }, error => {
